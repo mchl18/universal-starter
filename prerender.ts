ac4712e4f2ae531a93a2d35006955bb015c019df
Michael Gerullis <michael.gerullis.extern@idealo.de>
2020-01-24 15:26:07 +0100

    Create parallel prerendering

--- prerender.ts
+++ prerender.ts
@@ -1,43 +1,20 @@
-// Load zone.js for the server.
-import 'zone.js/dist/zone-node';
-import 'reflect-metadata';
-import {readFileSync, writeFileSync, existsSync, mkdirSync} from 'fs';
-import {join} from 'path';
+import { ROUTES } from './static.paths';
+import { from, Observable, Observer } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
+import { fork } from 'child_process';
 
-import {enableProdMode} from '@angular/core';
-// Faster server renders w/ Prod mode (dev mode never needed)
-enableProdMode();
+const concurrency = +process.argv[2] || 8;
 
-// Import module map for lazy loading
-import {provideModuleMap} from '@nguniversal/module-map-ngfactory-loader';
-import {renderModuleFactory} from '@angular/platform-server';
-import {ROUTES} from './static.paths';
+const observableChildProcess = (route) => new Observable((observer: Observer<string>) => {
+  const child = fork(`./render.js`, [route]);
+  child.on('message', (msg) =>observer.next(msg));
+  child.on('close', () => observer.complete())
+})
 
-// * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require('./server/main');
-
-const BROWSER_FOLDER = join(process.cwd(), 'browser');
-
-// Load the index.html file containing referances to your application bundle.
-const index = readFileSync(join('browser', 'index.html'), 'utf8');
-
-let previousRender = Promise.resolve();
-
-// Iterate each route path
-ROUTES.forEach(route => {
-  const fullPath = join(BROWSER_FOLDER, route);
-
-  // Make sure the directory structure is there
-  if (!existsSync(fullPath)) {
-    mkdirSync(fullPath);
-  }
-
-  // Writes rendered HTML to index.html, replacing the file if it already exists.
-  previousRender = previousRender.then(_ => renderModuleFactory(AppServerModuleNgFactory, {
-    document: index,
-    url: route,
-    extraProviders: [
-      provideModuleMap(LAZY_MODULE_MAP)
-    ]
-  })).then(html => writeFileSync(join(fullPath, 'index.html'), html));
-});
+from(ROUTES).pipe(
+  mergeMap(route => observableChildProcess(route), concurrency)
+).subscribe(
+  res => console.log(res),
+  err => console.error(err),
+  () => console.log('all done')
+);
